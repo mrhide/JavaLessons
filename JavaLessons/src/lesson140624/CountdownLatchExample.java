079250398796dda5d72f217b2fec83bf552ff2fd
Developer <flexdiez@yandex.ru>
2014-07-01 10:56:25 +0300

    2 files added

---
+++ JavaLessons/src/lesson140624/CountdownLatchExample.java
@@ -0,0 +1,55 @@
+package lesson140624;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import lesson140529.Utils;
+
+public class CountdownLatchExample {
+
+	static class Runner implements Runnable {
+		
+		private CountDownLatch _latch;
+
+		public Runner(CountDownLatch latch) {
+			_latch = latch;
+		}
+		
+		@Override
+		public void run() {
+			try {
+				System.out.println(this + " is ready");
+				_latch.await();
+				System.out.println(this + "(" + Thread.currentThread().getName() + ") " + " has started at " + System.nanoTime());
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		ExecutorService service = Executors.newCachedThreadPool();
+		
+		CountDownLatch latch = new CountDownLatch(3);
+		
+		service.execute(new Runner(latch));
+		service.execute(new Runner(latch));
+		service.execute(new Runner(latch));
+		service.execute(new Runner(latch));
+		
+		System.out.println("---");
+		
+		Utils.pause(3000);
+		
+		System.out.println("на старт!...");
+		latch.countDown();
+		Utils.pause(2000);
+		System.out.println("внимание!...");
+		latch.countDown();
+		Utils.pause(2000);
+		System.out.println("марш!");
+		latch.countDown();
+	}
+	
+}
---
+++ JavaLessons/src/lesson140624/CyclicBarriierExample.java
@@ -0,0 +1,54 @@
+package lesson140624;
+
+import java.util.Random;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import lesson140529.Utils;
+
+public class CyclicBarriierExample {
+	
+	static class Runner implements Runnable {
+		
+		private CyclicBarrier _barrier;
+
+		public Runner(CyclicBarrier barrier) {
+			_barrier = barrier;
+
+		}
+		
+		@Override
+		public void run() {
+			System.out.println(Thread.currentThread().getName() + " started..");
+			Random random = new Random();
+			Utils.pause(5000 + random.nextInt(5000));
+			System.out.println(Thread.currentThread().getName() + " finished.");
+			try {
+				_barrier.await();
+			} catch (InterruptedException | BrokenBarrierException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		public static void main(String[] args) {
+			CyclicBarrier barrier = new CyclicBarrier(4, new Runnable() {
+				@Override
+				public void run() {
+					System.out.println("all runners have finished!");
+				}
+			});
+			
+			ExecutorService service = Executors.newCachedThreadPool();
+			service.execute(new Runner(barrier));
+			service.execute(new Runner(barrier));
+			service.execute(new Runner(barrier));
+			service.execute(new Runner(barrier));
+			
+			System.out.println("await for all runners");
+			
+		}
+	}
+	
+}
