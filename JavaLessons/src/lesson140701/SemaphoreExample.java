c8ba7e8e083338ad5e54b0e52a9757e2da857365
Developer <flexdiez@yandex.ru>
2014-07-01 18:58:51 +0300

    lesson140701 init

---
+++ JavaLessons/src/lesson140701/SemaphoreExample.java
@@ -0,0 +1,26 @@
+package lesson140701;
+
+import java.util.concurrent.Semaphore;
+
+public class SemaphoreExample {
+	
+	static class Counter {
+		volatile int count;
+		final Semaphore sem = new Semaphore(1);
+		
+		
+		public int get() {
+			return count;
+		}
+		
+		public void inc() {
+			sem.acquireUninterruptibly();
+			try {
+				count++;				
+			} finally {
+				sem.release();
+			}
+		}
+	}
+	
+}
