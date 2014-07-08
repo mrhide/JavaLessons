24d52559ca6b0816e1ab276ab5ecb708a3c75467
Developer <flexdiez@yandex.ru>
2014-07-08 21:09:49 +0300

    StrinNumbers added

--- JavaLessons/src/lesson140708/StringNumbers.java
+++ JavaLessons/src/lesson140708/StringNumbers.java
@@ -1,9 +1,28 @@
 package lesson140708;
 
-public class StringNumbers {
+import java.util.HashMap;
+import java.util.Map;
 
+public class StringNumbers {
+	
+	static final Map<Integer, String> words = new HashMap<>();
+
+	static {
+		words.put(1, "один");
+		words.put(2, "два");
+		words.put(3, "три");
+		words.put(4, "четыре");
+		words.put(5, "пять");
+		words.put(6, "шесть");
+		words.put(7, "семь");
+		words.put(8, "восемь");
+		words.put(9, "девять");
+		words.put(10, "десять");
+	}
+	
 	public static String convert(int number) {
-		return "";
+		return words.get(number);
+		// закончить с использованием unit-тестов
 	}
 
 
--- JavaLessons/test/lesson140708/StringNumbersTest.java
+++ JavaLessons/test/lesson140708/StringNumbersTest.java
@@ -1,14 +1,44 @@
 package lesson140708;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(Parameterized.class)
 public class StringNumbersTest {
 
+	private int _number;
+	private String _expected;
+
+	@Parameters
+	public static Collection<Object[]> data() {
+		return Arrays.asList(new Object[][] {
+				{10, "десять"},
+				{1, "один"},
+				{21, "двадцать один"},
+				{2, "два"},
+				{3, "три"},				
+		});
+	}
+	
+	public StringNumbersTest(int number, String expected) {
+		_number = number;
+		_expected = expected;
+	}
+	
 	@Test
 	public void test() {
-		String Description = StringNumbers.convert(10);
+//		assertEquals("десять", StringNumbers.convert(10));
+//		assertEquals("один", StringNumbers.convert(1));
+//		assertEquals("двадцать один", StringNumbers.convert(21));
+	
+	assertEquals(_expected, StringNumbers.convert(_number));
 	}
 
 }
